465877d7d550120550d9dc18a82ddcf7fb18da64
lest210 <lest210@gmail.com>
2021-05-16 23:56:48 -0500

    added color to choices

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,18 +1,19 @@
 
 //could be const instead of var
-var question = document.getElementById("question");
-var choices = document.getElementsByClassName("choice-text");
-
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 //could be let instead of var
 
-var currentQuestion = {};
-var acceptingAnswers = {};
-var score = 0;
-var questionCounter = 0;
-var availableQuestions =[];
+let currentQuestion = {};
+let acceptingAnswers = {};
+let score = 0;
+let questionCounter = 0;
+let availableQuestions =[];
 
-var questions = [
+let questions = [
   {
   question: "Inside which HTML element do we put the JavaScript?",
   choice1: "<script>",
@@ -38,7 +39,7 @@ var questions = [
     choice3: "msg('Start Quiz');",
     choice4: "alert('Start Quiz');",
     answer:4
-    },
+    }
 
 
 ];
@@ -50,8 +51,60 @@ const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
-  questionCounter
-}
+
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+
+  if(availableQuestions.length ===0 || questionCounter > MAX_QUESTIONS) {
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++;
+  questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+  //maybe const instead of var
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question; 
+
+  choices.forEach(choice => {
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice' + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if(!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    
+
+    const classToApply = 
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+          selectedChoice.parentElement.classList.remove(classToApply);
+          getNewQuestion();
+        }, 1000);
+
+  });
+});
+
+startGame();
 
 
 
